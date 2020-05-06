f68b9e9483e3c66abd0142c49d5476280a17b5f1
treastrain / Tanaka Ryoga <tr@tret.jp>
2020-05-06 18:49:16 +0900

    Create Mockup project files for iOS App

---
+++ Mockups/iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultipleFireColumnMockup
+//
+//  Created by treastrain on 2020/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
